4cd8e72857fdb145deb807f200929c37f975fc8d
Ace <omarace95@gmail.com>
2022-02-12 12:22:23 -0800

    port issue resovled

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// connecting with MongoDB via mongoose at our myapp database running locally on default port 27017
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false 
+})
+    .then(() => console.log('MongoDB Database connected successfully.'))
+    .catch(err => console.log('Error:- ' + err ));
 
 // routes
 app.use(require("./routes/api.js"));
