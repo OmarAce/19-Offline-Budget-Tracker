7794738fc400519e9ec11632f24a232ec65620a7
Ace <omarace95@gmail.com>
2022-02-12 13:22:20 -0800

    finally found chart error

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/budget", {
     .catch(err => console.log('Error:- ' + err ));
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require(`./routes/api.js`));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
